ca97c2ea152006a6da10072cd794b6dd620ba026
suleimanmashini <33000035+suleimanmashini@users.noreply.github.com>
2017-10-30 19:54:58 +0000

    Working on the member functions, need to work out the endianness. Aim is to encapsulate the addresses conversion from the rest of the components

--- ram.cpp
+++ ram.cpp
@@ -7,15 +7,20 @@ RAM::RAM() {
 	RAM newRAM;
 }
 
-uint32_t RAM::getRAM(int addressIn) {
-	return address[addressIn];
+uint32_t RAM::getRAM(const int &addressIn) const {
+	int tempAddress = addressIn / 4;
+	return address[tempAddress];
 }
 
-void RAM::writeRAM(int addressIn, uint32_t dataIn) {
+void RAM::writeRAM(const int &addressIn,const uint32_t &dataIn) {
 	//THERE IS NO ADDRESS ERROR CHECKING ERROR CHECKING
+	int tempAddress = addressIn / 4;
+	address[tempAddress] = dataIn;
 }
 
-
-void RAM::writeRAM(int addressIn, uint8_t dataIn) {
-
+//NEED TO SORT OUT THE ENDIANNESS OF THIS*******************
+void RAM::writeRAM(const int &addressIn,const uint8_t &dataIn) {
+	//WRITE WITHIN A WORD ITSELF
+	uint32_t temp = address[addressIn / 4];
+	int shift = addressIn % 4;
 }
--- ram.hpp
+++ ram.hpp
@@ -9,10 +9,9 @@ public:
 	uint32_t address[805306376] = { 0 };
 private:
 	RAM();
-	uint32_t getRAM(int addressIn);
-	void writeRAM(int addressIn, uint32_t dataIn);
-	void writeRAM(int addressIn, uint8_t dataIn);
-	int addressToArrayInex(int addressIn);
+	uint32_t getRAM(const int &addressIn) const;
+	void writeRAM(const int &addressIn, const uint32_t &dataIn);
+	void writeRAM(const int &addressIn,const uint8_t &dataIn);
 };
 
 
