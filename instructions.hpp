537eaf2b9a1ec7440140b596230c9b098e854f8c
Victor <victor.sanchez@imperial.ac.uk>
2017-11-01 13:23:25 +0000

    describe functions

--- instructions.hpp
+++ instructions.hpp
@@ -1,2 +1,31 @@
+//THIS FILE DESCRIBES EVERY INSTURCTION
+//THAT MIPS SHOULD SUPPORT
 
-//headerzz
+#include "ram.hpp"
+
+uint32_t sll();
+uint32_t srl();
+uint32_t sra();
+uint32_t sllv();
+uint32_t srlv();
+uint32_t srav();
+uint32_t jr();
+uint32_t jalr();
+uint32_t mfhi();
+uint32_t mthi();
+uint32_t mflo();
+uint32_t mtlo();
+uint32_t mult();
+uint32_t multu();
+uint32_t div();
+uint32_t divu();
+uint32_t add();
+uint32_t addu();
+uint32_t sub();
+uint32_t subu();
+uint32_t and();
+uint32_t or();
+uint32_t xor();
+uint32_t nor();
+uint32_t slt();
+uint32_t sltu();
