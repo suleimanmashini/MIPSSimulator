28b95e663907bee7b2e8ca6ad88f1849b24d8316
Victor <victor.sanchez@imperial.ac.uk>
2017-11-02 10:12:41 +0000

    edit

--- instructions.hpp
+++ instructions.hpp
@@ -1,33 +1,33 @@
 //THIS FILE DESCRIBES EVERY INSTURCTION
 //THAT MIPS SHOULD SUPPORT
 
-#include "ram.hpp"
+#include "data.hpp"
 
 
 //R-type
-uint32_t sll(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t srl(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t sra(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t sllv(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t srlv(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t srav(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t jr(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t jalr(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t mfhi(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t mthi(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t mflo(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t mtlo(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t mult(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t multu(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t div(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t divu(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t add(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t addu(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t sub(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t subu(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t and(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t or(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t xor(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t nor(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t slt(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
-uint32_t sltu(uint32_t& rd[], uint32_t rs[], uint32_t rt[]);
+void sll(uint8_t rs, uint8_t rd, uint8_t rt);
+void srl(uint8_t rs, uint8_t rd, uint8_t rt);
+void sra(uint8_t rs, uint8_t rd, uint8_t rt);
+void sllv(uint8_t rs, uint8_t rd, uint8_t rt);
+void srlv(uint8_t rs, uint8_t rd, uint8_t rt);
+void srav(uint8_t rs, uint8_t rd, uint8_t rt);
+void jr(uint8_t rs, uint8_t rd, uint8_t rt);
+void jalr(uint8_t rs, uint8_t rd, uint8_t rt);
+void mfhi(uint8_t rs, uint8_t rd, uint8_t rt);
+void mthi(uint8_t rs, uint8_t rd, uint8_t rt);
+void mflo(uint8_t rs, uint8_t rd, uint8_t rt);
+void mtlo(uint8_t rs, uint8_t rd, uint8_t rt);
+void mult(uint8_t rs, uint8_t rd, uint8_t rt);
+void multu(uint8_t rs, uint8_t rd, uint8_t rt);
+void div(uint8_t rs, uint8_t rd, uint8_t rt);
+void divu(uint8_t rs, uint8_t rd, uint8_t rt);
+void add(uint8_t rs, uint8_t rd, uint8_t rt);
+void addu(uint8_t rs, uint8_t rd, uint8_t rt);
+void sub(uint8_t rs, uint8_t rd, uint8_t rt);
+void subu(uint8_t rs, uint8_t rd, uint8_t rt);
+void and(uint8_t rs, uint8_t rd, uint8_t rt);
+void or(uint8_t rs, uint8_t rd, uint8_t rt);
+void xor(uint8_t rs, uint8_t rd, uint8_t rt);
+void nor(uint8_t rs, uint8_t rd, uint8_t rt);
+void slt(uint8_t rs, uint8_t rd, uint8_t rt);
+void sltu(uint8_t rs, uint8_t rd, uint8_t rt);
