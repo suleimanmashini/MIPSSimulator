edf9e71c059d0e0ab48c877ca0a87f442977bdf7
Victor <victor.sanchez@imperial.ac.uk>
2017-11-04 16:24:07 +0000

    editing r-type

--- instructions.cpp
+++ instructions.cpp
@@ -1,1 +1,59 @@
-#include 
+#include "instructions.hpp"
+
+//shift left logical
+void sll(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t sa){
+    Register[rd] = rt << sa;
+}
+
+//shift right logical
+void srl(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t sa){
+    Register[rd] = rt >> sa;
+    Rgister[rd] & (0xFFFFFFFF >> sa);
+}
+
+//shift right arithmetic, default right shift c++
+void sra(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t sa){
+    Register[rd] = rt >> sa;
+}
+
+//shift left logical variable
+void sllv(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs){
+    Register[rd] = rs << rt;
+}
+
+//shift right logical variable
+void srlv(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs){
+    Register[rd] = rt >> rs;
+    Rgister[rd] & (0xFFFFFFFF >> rs);
+}
+
+//shift right arithmetic variable (default c++)
+void srav(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs){
+    Register[rd] = rt >> sa;
+}
+
+
+void jr();
+void jalr();
+void mfhi();
+void mthi();
+void mflo();
+void mtlo();
+void mult();
+void multu();
+void div();
+void divu();
+
+void add(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs){
+    Registe[rd] = rt + rs;
+}
+
+void addu();
+void sub();
+void subu();
+void and();
+void or();
+void xor();
+void nor();
+void slt();
+void sltu();
--- instructions.hpp
+++ instructions.hpp
@@ -5,33 +5,34 @@
 
 
 //R-type
-void sll();
-void srl();
-void sra();
-void sllv();
-void srlv();
-void srav();
+void sll(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t sa);
+void srl(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t sa);
+void sra(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t sa);
+void sllv(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs);
+void srlv(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs);
+void srav(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs);
 void jr();
-void jalr(uint8_t rs, uint8_t rd, uint8_t rt);
-void mfhi(uint8_t rs, uint8_t rd, uint8_t rt);
-void mthi(uint8_t rs, uint8_t rd, uint8_t rt);
-void mflo(uint8_t rs, uint8_t rd, uint8_t rt);
-void mtlo(uint8_t rs, uint8_t rd, uint8_t rt);
-void mult(uint8_t rs, uint8_t rd, uint8_t rt);
-void multu(uint8_t rs, uint8_t rd, uint8_t rt);
-void div(uint8_t rs, uint8_t rd, uint8_t rt);
-void divu(uint8_t rs, uint8_t rd, uint8_t rt);
-
-void add(uint32_t Register[32], int rd, int rs, int rt)
 
 
+void jalr();
+void mfhi();
+void mthi();
+void mflo();
+void mtlo();
+void mult();
+void multu();
+void div();
+void divu();
 
-void addu(uint8_t rs, uint8_t rd, uint8_t rt);
-void sub(uint8_t rs, uint8_t rd, uint8_t rt);
-void subu(uint8_t rs, uint8_t rd, uint8_t rt);
-void and(uint8_t rs, uint8_t rd, uint8_t rt);
-void or(uint8_t rs, uint8_t rd, uint8_t rt);
-void xor(uint8_t rs, uint8_t rd, uint8_t rt);
-void nor(uint8_t rs, uint8_t rd, uint8_t rt);
-void slt(uint8_t rs, uint8_t rd, uint8_t rt);
-void sltu(uint8_t rs, uint8_t rd, uint8_t rt);
+void add(uint32_t *Register, uint32_t rd, uint32_t rt, uint32_t rs);
+
+
+void addu();
+void sub();
+void subu();
+void and();
+void or();
+void xor();
+void nor();
+void slt();
+void sltu();
