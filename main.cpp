de0c3722cac96b88604f4da7fba105e69f031b5c
suleimanmashini <33000035+suleimanmashini@users.noreply.github.com>
2017-11-06 10:35:48 +0000

    Changedd the main, it should be able to correctly load binary files now and start the simulator.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 //SULEIMAN MASHINI, IONUT MOTOI, VICTOR SANCHEZS
 
 #include "data.hpp"
+#include "instructionDecode.hpp"
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -16,22 +17,25 @@ int main() {
 	cout << "Please input the binary file name" << endl;
 	cin >> inBinary;
 	inFile.open(inBinary.c_str(), ios::binary);
-	if (!inFile.is_open()) {
-		cout << "Invalid File";
-		exit (12); //EXIT IF INVALID FILE
-	}
-	else {
-		
+	if (inFile.is_open()) {
 		char inValue[4];
 		uint32_t tempInValue = 0;
 		int tempAddressImport = 0;
 		// obtain file size:
 		while (!inFile.eof()) {
 			inFile.read(inValue, 4);
-			tempAddressImport = 0;
-			tempInValue = (((((uint32_t)inValue[0]) << 24) | ((uint32_t)inValue[1]) << 16) | (((uint32_t)inValue[2]) << 8) | ((uint32_t)inValue[3]) );
+			//writing four chars into a single uint32 value
+			tempInValue = (((((uint32_t)inValue[0]) << 24) | ((uint32_t)inValue[1]) << 16) | (((uint32_t)inValue[2]) << 8) | ((uint32_t)inValue[3]));
 			mainMemory.writeRAM(INSTRUCTION_START_ADR + tempAddressImport, tempInValue);
+			tempAddressImport = tempAddressImport + 4;
 		}
+		inFile.close();
+		//THE SIMULATOR WILL NOW RUN
+		fetchIntstructions();
+	}
+	else {
+		cout << "Invalid File";
+		exit(12); //EXIT IF INVALID FILE
 	}
 	return 0;
 }
