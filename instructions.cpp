86ad33cc1d000c27bdfb15e3861fb2fa2f21b879
Victor <victor.sanchez@imperial.ac.uk>
2017-11-06 16:25:37 +0000

    rtype done + pc done

--- instructions.cpp
+++ instructions.cpp
@@ -3,105 +3,109 @@
 //shift left logical
 void sll(uint8_t rd, uint8_t rt, uint8_t shamt){
     Register[rd] = Register[rt] << shamt;
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //shift right logical
 void srl(uint8_t rd, uint8_t rt, uint8_t shamt){
     Register[rd] = Register[rt] >> shamt;
     Register[rd] & (0xFFFFFFFF >> shamt);
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //shift right arithmetic, default right shift c++
 void sra(uint8_t rd, uint8_t rt, uint8_t shamt){
     Register[rd] = Register[rt] >> shamt;
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //shift left logical variable
 void sllv(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = Register[rt] << Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //shift right logical variable
 void srlv(uint8_t rd, uint8_t rt, uint8_t rs){
     Register[rd] = Register[rt] >> Register[rs];
     Register[rd] & (0xFFFFFFFF >> Register[rs]);
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //shift right arithmetic variable (default c++)
 void srav(uint8_t rd, uint8_t rt, uint8_t rs){
     Register[rd] = Register[rt] >> Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //jump register
 void jr(uint8_t rs){
-    PC_increment(Register[rs];
+    PC_advance(Register[rs];
 }
 
-//not sure what this does
-void jalr();
+//jump and link (rtype)
+void jalr(uint8_t rd, uint8_t rs){
+    //save return address in $31
+    Register[RA] = Register[PC] + default_advance;
+    PC_advance(Register[rs]);
+}
 
 //move from hi
 void mfhi(uint8_t rd){
     Register[rd] = HI;
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //move to hi
 void mthi(uint8_t rs){
     Register[HI] = Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //move from low
 void mflo(uint8_t rd){
     Register[rd] = Register[LO];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //move to low
 void mtlo(uint8_t rs){
     Register[LO] = Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //overflow condition ?
 bool mult(uint8_t rs, uint8_t rt){
     Register[LO] = (Register[rs]*Register[rt]) << 0xFFFFFFFF;
     Register[HI] = (Register[rs]*Register[rt]) >> 0xFFFFFFFF;
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //unsigned mult
 void multu(uint8_t rs, uint8_t rt){
     Register[LO] = (Register[rs]*Register[rt]) << 0xFFFFFFFF;
     Register[HI] = (Register[rs]*Register[rt]) >> 0xFFFFFFFF;
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //overflow ?
 void div(uint8_t rs, uint8_t rt){
     Register[LO] = Register[rs] / Register[rt];
     Register[HI] = Register[rs] % Register[rt];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //unsigned division
 void divu(uint8_t rs, uint8_t rt){
     Register[LO] = Register[rs] / Register[rt];
     Register[HI] = Register[rs] % Register[rt];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //return 1 when overflow occurs
 bool add(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = Register[rt] + Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
     //different signs, no overflow possible
     if(Register[rd]*Register[rt] =< 0){ //***CHECK THAT UR STATEMENTS WORK!!
         return 0;
@@ -122,12 +126,12 @@ bool add(uint8_t rd, uint8_t rs, uint8_t rt){
 //basic add, no concern for overflow
 void addu(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = Register[rt] + Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 bool sub(uint8_t rd, uint8_t rt, uint8_t rs){
     Register[rd] = Register[rs] - Register[rt];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
     //same signs, no overflow possible
     if(Register[rd]*Register[rt] >= 0){
         return 0;
@@ -148,31 +152,31 @@ bool sub(uint8_t rd, uint8_t rt, uint8_t rs){
 //basic subtraction, no concern for overflow
 void subu(uint8_t rd, uint8_t rt, uint8_t rs){
     Register[rd] = Register[rs] - Register[rt];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //bitwise and
 void and(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = Register[rt] & Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //bitwise or
 void or(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = Register[rt] | Register[rs];
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //bitwise xor
 void xor(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = ((Register[rt] | Register[rs])&(~Register[rt] | ~Register[rs]))
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //bitwise nor
 void nor(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = ~(Register[rt] | Register[rs]);
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
     
 //set if less than equal
@@ -180,13 +184,13 @@ void slt(uint8_t rd, uint8_t rs, uint8_t rt){
     int8_t temp1 = (int8_t)Register[rs];
     int8_t temp2 = (int8_t)Register[rt];
     Register[rd] = (Register[rs]<Register[rt])? 1 : 0;
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
 
 //set if less than equal unsigned version
 void sltu(uint8_t rd, uint8_t rs, uint8_t rt){
     Register[rd] = (Register[rs] < Register[rt]) ? 1 : 0;
-    PC_increment(default_advance);
+    PC_advance(default_advance);
 }
     
     
