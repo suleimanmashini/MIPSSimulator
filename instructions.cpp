b25cbca4a1d448f3b33ac7d4b1fa4d36320f265f
suleimanmashini <33000035+suleimanmashini@users.noreply.github.com>
2017-12-08 14:39:55 +0000

    updated the jal test

--- instructions.cpp
+++ instructions.cpp
@@ -253,7 +253,7 @@ void jal(uint32_t target) {
 	if (DEBUG) cout << "jal" << endl;
 	Register[PC] += 4;
 	decodeInstructions();
-	Register[RA] = Register[PC] + 4; //added a plus 4
+	Register[RA] = Register[PC];
 	Register[PC] = (Register[PC] & 0xF0000000) + target;
 }
 
